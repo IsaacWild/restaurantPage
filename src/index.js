96ee7c1afca264ed0a2716d35bd003d3c5e39e30
Isaac <isaacwildsecond@gmail.com>
2022-05-15 15:51:53 +0100

    Header logo and title added and styled

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './style.css'
+import Icon from './dragonIcon.svg';
 
-console.log('test')
+//this is where my site will be born!
+const pageContent = document.querySelector("#content");
+const pageWrapper = document.createElement('div');
+pageWrapper.classList.add('pageWrapper');
+pageContent.appendChild(pageWrapper);
+
+const header = document.createElement('div');
+header.classList.add('header');
+pageWrapper.appendChild(header);
+
+const myIcon = new Image();
+myIcon.src = Icon;
+const logo = document.createElement('div');
+const title = document.createElement('div');
+const facebook = document.createElement('div');
+myIcon.classList.add('icon')
+logo.classList.add('logo');
+title.classList.add('title');
+facebook.classList.add('facebook')
+
+header.append(logo, facebook, title);
+logo.appendChild(myIcon);
+
+title.textContent = 'Welcome to Long Yuen Court';
+
+
+//make footer with (Icons by svgrepo.com) link to https://www.svgrepo.com/
