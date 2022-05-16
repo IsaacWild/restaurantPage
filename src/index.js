ca4afc0239b1494a7f565147b901f639746a57e1
Isaac <isaacwildsecond@gmail.com>
2022-05-16 18:23:02 +0100

    first selction completed with styling and link to menu page (not yet made)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './style.css'
 import Icon from './dragonIcon.svg';
+import menuIcon from './menuIcon.svg'
+import findIcon from './findIcon.svg'
+import contactIcon from './contactIcon.svg'
+
 import menuPage from './menu';
 
 //this is where my site will be born!
@@ -7,6 +11,7 @@ import menuPage from './menu';
 const pageWrapper = document.querySelector("#content");
 const headerWrapper = document.createElement('div');
 const titleWrapper = document.createElement('div');
+const removableSections = document.createElement('div');
 const menuWrapper = document.createElement('div');
 const findUsWrapper = document.createElement('div');
 const contactWrapper = document.createElement('div');
@@ -17,13 +22,16 @@ const footerWrapper = document.createElement('div');
 pageWrapper.classList.add('pageWrapper')
 headerWrapper.classList.add('headerWrapper');
 titleWrapper.classList.add('titleWrapper');
+removableSections.classList.add('removableSections')
 menuWrapper.classList.add('sectionWrapper');
 findUsWrapper.classList.add('sectionWrapper');
 contactWrapper.classList.add('sectionWrapper');
 footerWrapper.classList.add('footerWrapper');
 
 //Append time!
-pageWrapper.append(headerWrapper,titleWrapper,menuWrapper,findUsWrapper,contactWrapper,footerWrapper);
+pageWrapper.append(headerWrapper, titleWrapper, menuWrapper, removableSections, footerWrapper);
+removableSections.append(menuWrapper, findUsWrapper, contactWrapper)
+
 
 //headerWrapper Childern - logo
 const myIcon = new Image();
@@ -38,23 +46,59 @@ headerWrapper.append(myIcon, name);
 //titleWrapper Children - Welcome message
 const menuLink = document.createElement('p')
 menuLink.textContent = 'Menu'
-menuLink.addEventListener( 'click',() =>{
-    
+menuLink.addEventListener('click', () => {
+    removeChildren()
 })
 const findUsLink = document.createElement('p')
 findUsLink.textContent = 'Find Us'
-findUsLink.addEventListener( 'click',() =>{
-    
+findUsLink.addEventListener('click', () => {
+    removeChildren()
 })
 const contactLink = document.createElement('p')
 contactLink.textContent = 'Contact Us'
-contactLink.addEventListener( 'click',() =>{
-    
+contactLink.addEventListener('click', () => {
+    removeChildren()
 })
 
-titleWrapper.append(menuLink,findUsLink,contactLink)
+titleWrapper.append(menuLink, findUsLink, contactLink)
+
+function removeChildren() {
+    while (removableSections.firstChild) {
+        removableSections.removeChild(removableSections.lastChild);
+    }
+}
+
+//menuWrapper Children - Title, Pic, Message, Link
+//create children
+const menuTitle = document.createElement('h1')
+const menuImage = new Image()
+menuImage.src = menuIcon;
+const menuMessage = document.createElement('p')
+const menuSectionLink = document.createElement('p')
+//Add style
+menuImage.classList.add('menuImage')
+menuSectionLink.classList.add('menuSectionLink')
+//Add content
+menuTitle.textContent = 'Our Menu'
+menuMessage.textContent = "Our menu offers a large range of dishes hand crafted by our chef's and celibrated by our customers"
+menuSectionLink.textContent = 'MENU'
+menuSectionLink.addEventListener('click', () => {
+    removeChildren()
+    menuPage()
+})
+//Append Time
+menuWrapper.append(menuTitle, menuImage, menuMessage, menuSectionLink)
 
 
 
 
-//make footer with (Icons by svgrepo.com) link to https://www.svgrepo.com/
+//findUsWrapper Children - Title, Pic, Message, Link
+
+//contactUsWrapper Children - Title, Pic, Message, Link
+
+
+//footerWrapper Children - footer
+// const footer = document.createElement('a')
+// footer.textContent = 'Icons by svgrepo.com'
+// footer.href = 'https://www.svgrepo.com/'
+// footerWrapper.appendChild(footer)
